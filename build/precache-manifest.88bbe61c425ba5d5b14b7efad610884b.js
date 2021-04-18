5783af45df6c5ae940e7885c829f33d06f93c75e
Jennifer Ramsay <jenny.ramsay@gmail.com>
2021-04-18 12:05:19 +0100

    build folder

---
+++ build/precache-manifest.88bbe61c425ba5d5b14b7efad610884b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f039317dd47791843c3",
+    "url": "/static/js/main.3f039317.chunk.js"
+  },
+  {
+    "revision": "3501343630ed65e87ab3",
+    "url": "/static/js/1.35013436.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fc5b1991f83ea168e814a2d158943b7f",
+    "url": "/index.html"
+  }
+];
